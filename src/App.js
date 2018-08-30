09a8b5b65f99ac20e9ae79cd074112646015027f
Rina Hong <rhong1006@gmail.com>
2018-08-29 23:49:14 -0700

    Pkg installed for graphql setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 import './App.css';
+
+const apolloClient = new ApolloClient({});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={apolloClient}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+        </div>
+      </ApolloProvider>
     );
   }
 }
